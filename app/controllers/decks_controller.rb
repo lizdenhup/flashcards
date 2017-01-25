17689109ec453f9a476d91cc7c76a81bed692b5b
lizdenhup <lizdenhup@gmail.com>
2017-01-24 20:05:13 -0600

    checking UX flow

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,22 +1,24 @@
 class DecksController < ApplicationController
 
   def index
-    @decks = current_user.decks
+    @user = current_user
+    @decks = @user.decks
   end 
 
   def new
+    @user = current_user
     @deck = Deck.new
   end 
 
   def create
     @deck = Deck.new(deck_params)
     @deck.save 
-    redirect_to deck_path(@deck)
+    redirect_to user_deck_path(id: @deck.id, user_id: current_user.id)
   end 
 
   def show
     @deck = Deck.find_by(id: params[:id])
-    @cards = Card.all.where(deck_id: params[:deck_id]) 
+    @cards = Card.all.where(deck_id: params[:id]) 
   end 
 
   def update
