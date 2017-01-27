6ba27e1069b33d5ad29979ed3caf7fa660c93907
lizdenhup <lizdenhup@gmail.com>
2017-01-27 11:21:19 -0600

    added validations to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck 
+  validates :question, uniqueness: true
+  validates :answer, uniqueness: true 
 
-  accepts_nested_attributes_for :deck 
-  
+  accepts_nested_attributes_for :deck   
 end
