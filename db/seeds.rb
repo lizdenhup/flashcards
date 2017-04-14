e502d94ac82f9dd7528290992da5f5c6bdbd1d16
lizdenhup <lizdenhup@gmail.com>
2017-04-14 15:18:52 -0500

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+states = ["Alabama" "Alaska", "Arizona", "Arkansas", "California", "Colorado",
+"Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+"Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+"New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+"Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+"Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin",
+"Wyoming"]
+
+capitals = ["Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford",
+"Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines",
+"Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul",
+"Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe",
+"Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence",
+"Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia",
+"Charleston", "Madison", "Cheyenne"]
+
+@subject = Subject.create(name: "Geography")
+Deck.create(name: "States and their Capitals", user_id: User.first.id, subject_id: @subject.id)
