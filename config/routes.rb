1e42dc9a28b6efe50b67baf656142f1c4bd46edb
lizdenhup <lizdenhup@gmail.com>
2017-04-17 16:27:38 -0500

    some progress in rendering cards index via $.get

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
       resources :cards, except: :index 
     end
   end 
-
-  get '/users/:user_id/decks/:deck_id/cards/:id/card_data', to: 'cards#card_data'
-
+ 
   get '/academic_leaderboard', :to => 'welcome#academic_leaderboard_show'
 
 resources :subjects 
